01db72f07efcd9b18d6739d9da29bd6eeb4fdd8c
Student Alexandr <alexandr110282@yandex.ru>
2022-09-18 21:49:28 +0300

    Добавлены новые рабочие папки

---
+++ lesson2/Program.cs
@@ -0,0 +1,21 @@
+﻿// Максимальное трех RANDOM чисел
+
+Console.Clear();
+int number1 = new Random().Next (1, 101);
+Console.WriteLine(number1);
+int number2 = new Random().Next (1, 101);
+Console.WriteLine(number2);
+int number3 = new Random().Next (1, 101);
+Console.WriteLine(number3);
+
+int max = number1;
+
+if (number2 > max)
+{
+    max = number2;
+}
+if (number3 > max)
+{
+    max = number3;
+}
+Console.WriteLine($"Максимальное число {max}");
